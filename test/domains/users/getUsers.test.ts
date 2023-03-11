879bf5f08e93a99898b6b045b0a2a75da3401206
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-11 18:27:12 +0000

    c: rm consoleLogs and extra code in getUsers test

--- test/domains/users/getUsers.test.ts
+++ test/domains/users/getUsers.test.ts
@@ -1,6 +1,6 @@
 import { connect } from '../../../src/';
 
-describe('getUsers', () => {
+describe('doesWork', () => {
     it('it works!', async () => {
         const { doesWork } = connect(`${process.env.LMNSQZY_API_TOKEN}`);
         expect(doesWork()).toBeTruthy()
