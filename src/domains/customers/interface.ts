06523fce8d82211846211d52189aac43c9ae17c8
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-12 13:46:05 +0000

    c: created `IGetCustomer`

--- src/domains/customers/interface.ts
+++ src/domains/customers/interface.ts
@@ -1,1 +1,38 @@
-export interface IGetCustomers {}
+import { domains } from "~/general"
+
+export interface IGetCustomer<ILink = {
+    self: string
+}> {
+    type: domains
+    id: string
+    attributes: {
+        'store_id': number,
+        name: string
+        slug: string
+        description: string
+        status: string
+        'status_formatted': string
+        'thumb_url': string
+        'large_thumb_url': string
+        'price': number
+        'pay_what_you_want': false
+        'from_price': number | null
+        'to_price': number | null
+        'buy_now_url': string
+        'price_formatted': string
+        'created_at': string
+        'updated_at': string
+    },
+    relationships: IGetStoreRelationships,
+    links: ILink
+}
+
+interface IGetStoreRelationships<ILinks = {
+    links: {
+        related: string;
+        self: string;
+    }
+}> {
+    store: ILinks
+    variants: ILinks
+}
