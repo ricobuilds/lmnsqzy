31cacf3cfd3e5de42070f1dbb6af4df524a88886
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 13:34:29 +0000

    c: added some tests for `doesWork`

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,8 +2,19 @@ import { describe, it } from 'vitest';
 import { connect } from '../src';
 
 describe('doesWork', () => {
-  it('it works!', async () => {
+  it('works', async () => {
     const { doesWork } = connect(`${process.env.LMNSQZY_API_TOKEN}`);
-    expect(doesWork()).toBeTruthy();
+    let z = doesWork()
+    expect(z).toBeTruthy();
+  });
+  it('returns a value', async () => {
+    const { doesWork } = connect(`${process.env.LMNSQZY_API_TOKEN}`);
+    let z = doesWork()
+    expect(z).toBe('it works!');
+  });
+  it(`doesn't return an empty string`, async () => {
+    const { doesWork } = connect(`${process.env.LMNSQZY_API_TOKEN}`);
+    let z = doesWork()
+    expect(z).not.toBe('');
   });
 });
