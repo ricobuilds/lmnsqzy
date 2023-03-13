91eee1c7212da8a1b7f635526f84a478459e85c1
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 11:45:27 +0000

    c: created `LicenseKeyInstance` interface

--- src/domains/license-key-instances/interface.ts
+++ src/domains/license-key-instances/interface.ts
@@ -1,1 +1,13 @@
-export interface LicenseKeyInstance {}
+import { domains } from "~/general";
+
+export interface LicenseKeyInstance {
+    "type": domains,
+    "id": string,
+    "attributes": {
+        "license_key_id": number,
+        "identifier": string,
+        "name": string,
+        "created_at": string,
+        "updated_at": string
+    },
+}
