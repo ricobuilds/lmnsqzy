f5b02477bb90111924bf5cdab12c5f11d2e770a0
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 18:12:33 +0000

    added issue `#1`

--- test/domains/checkouts/createCheckout.test.ts
+++ test/domains/checkouts/createCheckout.test.ts
@@ -5,6 +5,7 @@ describe('createCheckout', () => {
   it('it works!', async () => {
     const { lmnsqzy } = connect(`${process.env.LMNSQZY_API_TOKEN}`);
     const { createCheckout } = lmnsqzy();
+    // TODO: #1 Create a type for the input as per la https://docs.lemonsqueezy.com/api/checkouts#create-a-checkout
     let z = await createCheckout();
     expect(z).toBeTruthy();
   });
