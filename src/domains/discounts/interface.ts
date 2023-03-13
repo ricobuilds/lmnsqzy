3cd4e7952d699825fff7f098e6b4874409d4c0b8
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 11:49:59 +0000

    c: created `Discount` interface

--- src/domains/discounts/interface.ts
+++ src/domains/discounts/interface.ts
@@ -1,1 +1,24 @@
-export interface Discount {}
+import { domains } from "~/general"
+
+export interface Discount {
+    "type": domains,
+    "id": string,
+    "attributes": {
+        "store_id": number,
+        "name": string,
+        "code": string,
+        "amount": number,
+        "amount_type": string,
+        "is_limited_to_products": boolean,
+        "is_limited_redemptions": boolean,
+        "max_redemptions": number,
+        "starts_at": string | null,
+        "expires_at": string | null,
+        "duration": string,
+        "duration_in_months": number,
+        "status": string,
+        "status_formatted": string,
+        "created_at": string,
+        "updated_at": string
+    }
+}
