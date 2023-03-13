aef75b4735e300c83497e24d251551f4a9971202
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 22:10:08 +0000

    modded `getDiscounts` to generics

--- src/types.ts
+++ src/types.ts
@@ -232,7 +232,7 @@ export type TMethods = {
    * @docs Refer to: https://docs.lemonsqueezy.com/api/customers
    * @returns a set of `discounts` objects, ordered by created_at field in descending order.
    */
-  getDiscounts(): Promise<GetDiscountsResponse>;
+  getDiscounts<R>(): Promise<R>;
 
   /**
    * This handler gets a discount redemption by the ID.
