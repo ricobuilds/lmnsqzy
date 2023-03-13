aab1d2012a396e62419777ac48c2780847fc13e0
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 11:48:30 +0000

    c: created `DiscountRedemption` interface

--- src/domains/discount-redemptions/interface.ts
+++ src/domains/discount-redemptions/interface.ts
@@ -1,1 +1,17 @@
-export interface DiscountRedemption {}
+import { domains } from "~/general"
+
+export interface DiscountRedemption {
+    "type": domains,
+    "id": string,
+    "attributes": {
+        "discount_id": number,
+        "order_id": number,
+        "discount_name": string,
+        "discount_code": string,
+        "discount_amount": number,
+        "discount_amount_type": string,
+        "amount": number,
+        "created_at": string,
+        "updated_at": string
+    }
+}
