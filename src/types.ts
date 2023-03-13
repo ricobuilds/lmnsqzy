2c485942b01bf13c6217b8fbd48348981607c6bd
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 22:05:22 +0000

    modded `getFiles` to generics

--- src/types.ts
+++ src/types.ts
@@ -125,7 +125,7 @@ export type TMethods = {
    * @docs Refer to: https://docs.lemonsqueezy.com/api/customers
    * @returns a set of `file` objects, ordered by created_at field in descending order.
    */
-  getFiles(): Promise<GetFilesResponse>;
+  getFiles<R>(): Promise<R>;
 
   /**
    * This handler gets an order by the ID.
