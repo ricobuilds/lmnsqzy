fbc17f128aad594401bb010f501797bc8e04a7b6
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 09:56:39 +0000

    c: added `getSubscriptions`

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { GetVariantResponse, GetVariantsResponse } from './domains/variants';
 import { GetFileResponse, GetFilesResponse } from './domains/files';
 import { GetOrderResponse, GetOrdersResponse } from './domains/orders';
 import { GetOrderItemResponse, GetOrderItemsResponse } from './domains/order-items';
-import { GetSubscription, UpdateSubscription } from './domains/subscriptions';
+import { GetSubscription, GetSubscriptions, UpdateSubscription } from './domains/subscriptions';
 
 /**
  * This function helps you connect to the API endpoints.
@@ -413,8 +413,26 @@ export const connect = (token: string): TLmnsqzyFunctions => {
 
     return r.json() as Promise<GetSubscription>
   }
-  async function getSubscriptions(): Promise<string> {
-    return ``;
+  async function getSubscriptions(): Promise<GetSubscriptions> {
+    let r = await fetch(`${constants.LMNSQZY_BASE_URL}/v1/subscriptions`, {
+      method: 'GET',
+      headers: {
+        ...constants.LMNSQZY_HEADERS,
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    if (!r.ok) {
+      const errors: GetOrderItemsResponse = {
+        jsonapi: {
+          version : '1.0'
+        },
+        errors: errorsTable[r.status]
+      }
+      return errors
+    }
+
+    return r.json() as Promise<GetSubscriptions>
   }
   async function cancelSubscription(): Promise<string> {
     return ``;
