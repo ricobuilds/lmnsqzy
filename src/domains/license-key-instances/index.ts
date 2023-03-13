a7aef66cae3e07c8de55684f6219d4db4dc1696b
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 16:19:46 +0000

    converted `LicenseKeyInstance` types to interfaces

--- src/domains/license-key-instances/index.ts
+++ src/domains/license-key-instances/index.ts
@@ -1,9 +1,5 @@
-import { LmnsqzyError, LmnsqzyResponse } from '~/general/general.responses';
+import { LmnsqzyResponse } from '~/general/general.responses';
 import { LicenseKeyInstance } from './interface';
 
-export type GetLicenseKeyInstanceResponse =
-  | LmnsqzyResponse<LicenseKeyInstance>
-  | LmnsqzyError;
-export type GetLicenseKeyInstancesResponse =
-  | LmnsqzyResponse<LicenseKeyInstance[]>
-  | LmnsqzyError;
+export interface GetLicenseKeyInstanceResponse extends LmnsqzyResponse<LicenseKeyInstance>{};
+export interface GetLicenseKeyInstancesResponse extends LmnsqzyResponse<LicenseKeyInstance[]>{};
