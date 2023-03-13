658679c93efc52c40a204f1ec6e0968ad45cf226
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 11:47:15 +0000

    c: created `LicenseKey` interface

--- src/domains/license-keys/interface.ts
+++ src/domains/license-keys/interface.ts
@@ -1,1 +1,24 @@
-export interface LicenseKey {}
+import { domains } from "~/general";
+
+export interface LicenseKey {
+    "type": domains,
+    "id": string,
+    "attributes": {
+        "store_id": number,
+        "order_id": number,
+        "order_item_id": number,
+        "product_id": number,
+        "user_name": string,
+        "user_email": string,
+        "key": string,
+        "key_short": string,
+        "activation_limit": number,
+        "instances_count": number,
+        "disabled": number,
+        "status": string,
+        "status_formatted": string,
+        "expires_at": string | null,
+        "created_at": string,
+        "updated_at": string
+    },
+}
