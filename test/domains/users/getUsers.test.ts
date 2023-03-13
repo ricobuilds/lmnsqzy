5177f41a6fe0b6b6c36896e3e35af3f60540a299
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 13:39:23 +0000

    c: added test for getUser

--- test/domains/users/getUsers.test.ts
+++ test/domains/users/getUsers.test.ts
@@ -1,10 +1,17 @@
 import { describe, it } from 'vitest';
 import { connect } from '../../../src/';
+import { GetUserResponse } from '../../../src/domains/users';
 
-describe('doesWork', () => {
-  it('it works!', async () => {
+describe('getUser', () => {
+  it('works', async () => {
     const { lmnsqzy } = connect(`${process.env.LMNSQZY_API_TOKEN}`);
     const { getUser } = lmnsqzy();
     expect(getUser()).toBeTruthy();
+  });
+  it('works', async () => {
+    const { lmnsqzy } = connect(`${process.env.LMNSQZY_API_TOKEN}`);
+    const { getUser } = lmnsqzy();
+    const z: GetUserResponse = await getUser()
+    expect(z.data.attributes.name).toBeTruthy();
   });
 });
