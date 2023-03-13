8a7bf4f620fb10c7960a124e656af0dc2e0173e2
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 12:04:02 +0000

    c: updated `User` interface

--- src/domains/users/interface.ts
+++ src/domains/users/interface.ts
@@ -3,7 +3,15 @@ import { domains } from '../../general';
 export interface User {
   type: domains;
   id: string;
-  attributes: {};
+  attributes: {
+    name: string
+    email: string
+    color: string
+    'avatar_url': string
+    'has_custom_avatar': boolean
+    'createdAt': string
+    'updatedAt': string
+  };
   links: {
     self: string;
   };
