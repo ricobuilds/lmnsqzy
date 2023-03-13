670f06f6f87faced5c11dd7fc546cf80f423c1bd
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 11:54:59 +0000

    c: created `OrderItem` interface

--- src/domains/order-items/interface.ts
+++ src/domains/order-items/interface.ts
@@ -1,1 +1,16 @@
-export interface OrderItem {}
+import { domains } from "~/general"
+
+export interface OrderItem {
+    "type": domains,
+    "id": string
+    "attributes": {
+        "order_id": number,
+        "product_id": number,
+        "variant_id": number,
+        "product_name": string,
+        "variant_name": string,
+        "price": number,
+        "created_at": string,
+        "updated_at": string
+    }
+}
