5fb96da142795f4286e89937d15938bc431fd39d
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 12:26:35 +0000

    c: updated `OrderItem` interface to match API response

--- src/domains/order-items/interface.ts
+++ src/domains/order-items/interface.ts
@@ -13,4 +13,20 @@ export interface OrderItem {
         "created_at": string,
         "updated_at": string
     }
+    relationships: Relationships
+    links: {
+        self: string
+    }
 }
+
+interface Relationships<Links = {
+    links: {
+      related: string
+      self: string
+    }
+  }> {
+    order: Links
+    product: Links
+    variant: Links
+  }
+  
