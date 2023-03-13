04d3b8821a043b632e7d92ccf1d60146baf9a341
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 14:20:58 +0000

    created `CheckoutOptions` interface

--- src/domains/checkouts/index.ts
+++ src/domains/checkouts/index.ts
@@ -4,3 +4,11 @@ import { Checkout } from './interface';
 export type CreateCheckoutResponse = LmnsqzyResponse<Checkout> | LmnsqzyError;
 export type GetCheckoutResponse = LmnsqzyResponse<Checkout> | LmnsqzyError;
 export type GetCheckoutsResponse = LmnsqzyResponse<Checkout[]> | LmnsqzyError;
+
+export interface CheckoutOptions {
+  name: string
+  code: string
+  amount: number
+  'amount_type': string
+  'store_id': string
+}
