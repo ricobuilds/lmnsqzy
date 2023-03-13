21138c14631e6ac3c57856d1b17aa09e60729343
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-13 16:22:49 +0000

    converted `Store` types to interfaces

--- src/domains/stores/index.ts
+++ src/domains/stores/index.ts
@@ -1,5 +1,5 @@
-import { LmnsqzyError, LmnsqzyResponse } from '~general/general.responses';
+import { LmnsqzyResponse } from '~general/general.responses';
 import { Store } from './interface';
 
-export type GetStoreResponse = LmnsqzyResponse<Store> | LmnsqzyError;
-export type GetStoresResponse = LmnsqzyResponse<Store[]> | LmnsqzyError;
+export interface GetStoreResponse extends LmnsqzyResponse<Store> {};
+export interface GetStoresResponse extends LmnsqzyResponse<Store[]> {};
